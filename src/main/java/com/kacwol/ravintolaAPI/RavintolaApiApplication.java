ee49f3fa2f40efb143db04949da478bdc5387f19
Kacper Wolinski <kacperwolinski404@gmail.com>
2022-08-25 13:18:34 +0200

    initial commit

---
+++ src/main/java/com/kacwol/ravintolaAPI/RavintolaApiApplication.java
@@ -0,0 +1,13 @@
+package com.kacwol.ravintolaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavintolaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavintolaApiApplication.class, args);
+	}
+
+}
